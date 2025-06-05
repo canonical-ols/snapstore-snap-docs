237b28e83f40d3fb2037784654ea777971ec1513
secondskoll <michael.park@canonical.com>
2025-06-05 14:35:13 +1000

    update logic

--- docs/conf.py
+++ docs/conf.py
@@ -182,7 +182,7 @@ html_baseurl = 'https://documentation.ubuntu.com/enterprise-store/'
 # When configured with RTD variables, check for RTD environment so manual runs succeed:
 
 if 'READTHEDOCS_VERSION' in os.environ:
-    version = f'{os.environ["READTHEDOCS_VERSION"]}'
+    version = os.environ["READTHEDOCS_VERSION"]
     sitemap_url_scheme = '{version}{link}'
 else:
     sitemap_url_scheme = 'VERSION/{link}'
